5d60a6f4a1d7a998aa93a4f9b5035711628fa7b8
root <achalla@allchicago.org>
2018-05-17 11:47:40 -0700

    created all forms/views

--- user_app/forms.py
+++ user_app/forms.py
@@ -4,4 +4,26 @@ class RegistrationForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     phone = forms.IntegerField()
-    
+
+class CityForm(forms.Form):
+    id = forms.IntegerField()
+    name = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea)
+
+class ArticleForm(forms.Form):
+    id = forms.IntegerField()
+    name = forms.CharField()
+    title = forms.CharField()
+    link_to_pdf = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea)
+    user_id = forms.IntegerField()
+
+class OrganizationForm(forms.Form):
+    id = forms.IntegerField()
+    name = forms.CharField()
+    address = forms.CharField()
+    cityID = forms.IntegerField()
+
+class TagForm(forms.Form):
+    id = forms.IntegerField()
+    keyword = forms.CharField()
--- user_app/views.py
+++ user_app/views.py
@@ -8,4 +8,19 @@ def registration_form_view(request):
     form = forms.RegistrationForm()
     return render(request,'forms/registration_form.html',{'form':form})
 
+def city_form_view(request):
+    form = forms.CityForm()
+    return render(request,'forms/city_form.html',{'form':form})
+
+def article_form_view(request):
+    form = forms.ArticleForm()
+    return render(request,'forms/aticle_form.html',{'form':form})
+
+def organization_form_view(request):
+    form = forms.OrganizationForm()
+    return render(request,'forms/organization_form.html',{'form':form})
+
+def tag_form_view(request):
+    form = forms.TagForm()
+    return render(request,'forms/tag_form.html',{'form':form})
 # Create your views here.
